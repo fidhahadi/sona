a2702f0b7009ceb7577a8fc7034349307b14cb57
Fidha Rafeeque <fidharafeequen@gmail.com>
2023-09-14 10:10:02 +0400

    resp

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,7 +4,7 @@ import cookware from './cookware.jpeg';
 
 const Products = () => {
   return (
-    <div className="px-44 py-20">
+    <div className="lg:px-44 lg:py-20">
       <h1 className="text-center text-5xl font-bold">Products</h1>
       <br/>
       <p className="text-center">Sona Gift N' Crockery is the perfect destination for kitchen and home décor. Whether you are looking for kitchenware, cookware, dinner sets, flower pots, biodegradable plastics, containers, kitchen appliances, chairs, kitchen utensils, or anything else related to kitchen and home décor, you will find it all here. We are committed to providing our customers with the highest quality products and excellent customer service. Visit us today and explore our wide range of products and unique designs.</p>
