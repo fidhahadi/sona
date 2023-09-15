e88cbd8fb637542fc982f0b2650ba83fcbac6910
Fidha Rafeeque <fidharafeequen@gmail.com>
2023-09-15 22:04:51 +0400

    changes

--- src/components/PartnerFirms/PartnerFirms.jsx
+++ src/components/PartnerFirms/PartnerFirms.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
-import sonaimage from './top-view-dining-tables-without-food.jpg';
+import sonaimage from './sona.jpg';
 import sonyimage from './sony.jpg';
-
+import bestimage from './best.jpg';
 const PartnerFirms = () => {
   return (
     <div className="lg:flex">
         <div class="max-w-lg bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <img class="rounded-t-lg" src={sonaimage} alt="" />
+        
          <div class="p-5">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Sona Crockery </h5>
 
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">At Sona Crockery, we specialize in providing quality kitchenware and electronic appliances. Our store has a wide range of items, from school items to kitchenware to electronic appliances. We also have a retail counter where customers can purchase directly from us.<br />
         Our mission is to make sure our customers are always satisfied with the products and services that we provide. We strive to bring our customers the best items at the best prices, with a focus on quality and customer service. Visit us today to see what we have to offer..</p> 
+        <img class="rounded-t-lg" src={sonaimage} alt="" />
     </div>
 </div>
 
@@ -27,7 +28,7 @@ We also have a godown facility where all our products are stored and shipped on 
 
 
 <div class="max-w-lg bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <img class="rounded-t-lg" src={sonaimage} alt="" />
+        
          <div class="p-5">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Best Agencies</h5>
 
@@ -35,6 +36,7 @@ We also have a godown facility where all our products are stored and shipped on 
 
 Our team is comprised of experienced personnel who are passionate about delivering products and services that meet our client’s needs. We strive to provide our customers with the best service possible and ensure that all our products are safe and reliable.</p> 
     </div>
+    <img class="rounded-t-lg" src={bestimage} alt="" />
 </div>
 
 
