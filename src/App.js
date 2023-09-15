17493655476f7dad211187d320c77470931cfb32
Fidha Rafeeque <fidharafeequen@gmail.com>
2023-09-15 22:19:46 +0400

    whatsapp chat enabled

--- src/App.js
+++ src/App.js
@@ -5,21 +5,32 @@ import AboutUs from "./components/AboutUs/AboutUs";
 import Products from './components/Products/Products';
 import HomePage from "./components/HomePage/HomePage";
 import PartnerFirms from "./components/PartnerFirms/PartnerFirms";
+import ContactUs from "./components/ContactUs/ContactUs";
+import './App.css';
 
 
 
 function App() {
-  return (
-    <Router>
-      <NavBar1 />
-      <Routes>
-        <Route path='/home' element={<HomePage />} />
-        <Route path='/aboutus' element={<AboutUs />} />
-        <Route path='/products' element={<Products />} />
-        <Route path='/partnerfirms' element={<PartnerFirms />} />
+  return (<>    <Router>
+    <NavBar1 />
+    <Routes>
+      <Route path='/home' element={<HomePage />} />
+      <Route path='/aboutus' element={<AboutUs />} />
+      <Route path='/products' element={<Products />} />
+      <Route path='/partnerfirms' element={<PartnerFirms />} />
+      <Route path='/contactus' element={<ContactUs />} />
+    </Routes>
+  </Router>
+    <a
+      href="https://wa.me/918075868738"
+      class="whatsapp_float"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <i class="fa fa-whatsapp whatsapp-icon"></i>
+    </a>
 
-      </Routes>
-    </Router>
+  </>
   );
 }
 
---
+++ src/components/ContactUs/ContactUs.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './ContactUs.css';
+
+const ContactUs = () => {
+  return (
+    <div>ContactUs
+        <div>
+            {/* WhatsApp icon */}
+      
+        </div>
+    </div>
+  )
+}
+
+export default ContactUs
